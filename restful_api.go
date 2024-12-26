e6a83393bb01c73d13e82f7a82a2a8aac0afb978
Arnav <113816479+arnavj98@users.noreply.github.com>
2024-12-26 11:17:35 +0530

    restful_api.go

--- restful_api.go
+++ restful_api.go
@@ -22,8 +22,21 @@ func getTodos(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todos)
 }
 
+func addTodos(context *gin.Context) {
+	var newTodo todo
+
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
+
+	todos = append(todos, newTodo)
+
+	context.IndentedJSON(http.StatusCreated, newTodo)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
+	router.POST("/todos", addTodos)
 	router.Run("localhost:9090")
 }
